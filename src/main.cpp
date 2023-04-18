5637e5f1137316fca6af39a4d1ca9f4c71d2ef33
Frostbyte8 <51795393+Frostbyte8@users.noreply.github.com>
2023-04-18 15:22:25 -0500

    Change flags to stop MSVC from reporting false postive memory leaks.

--- src/main.cpp
+++ src/main.cpp
@@ -18,8 +18,9 @@
 
 	int APIENTRY WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		int retval = DebugRun();
+        _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 		_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG); 
-		_CrtDumpMemoryLeaks();
+		//_CrtDumpMemoryLeaks();
 		return retval;
 	}
 #else
